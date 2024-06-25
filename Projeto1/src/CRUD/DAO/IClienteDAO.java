a8cf4357d1e4bfcfc09e766073632c0a81b3be54
Philip <72715163+phpnogueira@users.noreply.github.com>
2024-06-25 18:16:24 -0300

    Versionando o Projeto1 da Ebac

---
+++ Projeto1/src/CRUD/DAO/IClienteDAO.java
@@ -0,0 +1,19 @@
+package CRUD.DAO;
+
+import CRUD.Domain.Cliente;
+
+import java.util.Collection;
+
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public  Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+
+}
